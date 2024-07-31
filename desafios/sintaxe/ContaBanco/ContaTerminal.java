bb63510bc4356031a9f85acfe5bd4e0dd7eaedd3
Ubiratan Tavares <ust1973@gmail.com>
2024-07-31 17:55:33 -0300

    inserindo o TODO na classe ContaTerminal

--- desafios/sintaxe/ContaBanco/ContaTerminal.java
+++ desafios/sintaxe/ContaBanco/ContaTerminal.java
@@ -1,5 +1,15 @@
 public class ContaTerminal {
     public static void main(String[] args) {
+
+        //TODO: Conhecer e importar a classe Scanner
+
+        //TODO: Exibir as mensagens para o nosso usuário
+        
+        //TODO: Obter pela classe Scanner os valores digitados no terminal
+
+        //TODO: Exibir a mensagem solicitada
+
+        
         
     }
 }
